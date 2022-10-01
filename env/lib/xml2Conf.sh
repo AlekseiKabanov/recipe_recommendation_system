5bb89b120f5e3db42f8ed8f8975dbc6eff2d7218
Alex <alex@MacBook-Pro-Alex.local>
2022-10-01 15:22:29 +0300

    Starting the project

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/alex/Desktop/rec_syst/env/lib"
+XML2_LIBS="-lxml2 -L/Users/alex/Desktop/rec_syst/env/lib -lz -L/Users/alex/Desktop/rec_syst/env/lib -llzma -lpthread -L/Users/alex/Desktop/rec_syst/env/lib -liconv -L/Users/alex/Desktop/rec_syst/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/alex/Desktop/rec_syst/env/include/libxml2 -I/Users/alex/Desktop/rec_syst/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
---
+++ env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/alex/Desktop/rec_syst/env/lib"
+XSLT_LIBS="-lxslt -L/Users/alex/Desktop/rec_syst/env/lib -lxml2 -L/Users/alex/Desktop/rec_syst/env/lib -L/Users/alex/Desktop/rec_syst/env/lib -lpthread -L/Users/alex/Desktop/rec_syst/env/lib -liconv -L/Users/alex/Desktop/rec_syst/env/lib -licui18n -licuuc -licudata -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/alex/Desktop/rec_syst/env/include"
+MODULE_VERSION="xslt-1.1.35"
